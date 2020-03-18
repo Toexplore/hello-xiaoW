c63a7d7b1ec762184fb081241775f6bc7b874cfd
solar.wf <solar.wf@alibaba-inc.com>
2020-03-19 00:07:43 +0800

    init springboot & jsp framework for aning

---
+++ src/main/java/com/atguigu/sixsixsix/SixsixsixApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.sixsixsix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SixsixsixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SixsixsixApplication.class, args);
+	}
+
+}
